0477097ec566a927b67f7b74d14b5f723769c476
Guillaume <gcolsenet@coreoz.com>
2021-10-13 20:46:10 +0200

    test on exceptions

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,31 +1,30 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-
-describe("Gilded Rose basic setting", function() {
+describe("Gilded Rose basic setting", function () {
   let items = [new Item("basic item", 10, 20)];
-  let gildedRose = new Shop();  
+  let gildedRose = new Shop();
 
-  beforeEach(()  => {
+  beforeEach(() => {
     // name, sellIn, quality
     items = [new Item("basic item", 10, 20)];
     gildedRose = new Shop(items);
-  })
+  });
 
-  it("test premier, initialisation of a basic Item", function() {
+  it("test premier, initialisation of a basic Item", function () {
     expect(items[0].name).toBe("basic item");
     expect(items[0].sellIn).toBe(10);
     expect(items[0].quality).toBe(20);
   });
 
-  it("day 1", function() {
+  it("day 1", function () {
     items = gildedRose.updateQuality();
     expect(items[0].name).toBe("basic item");
     expect(items[0].sellIn).toBe(9);
     expect(items[0].quality).toBe(19);
   });
 
-  it("day 10", function() {
-    for(let day = 0; day < 10; day++){
+  it("day 10", function () {
+    for (let day = 0; day < 10; day++) {
       items = gildedRose.updateQuality();
     }
     expect(items[0].name).toBe("basic item");
@@ -34,18 +33,18 @@ describe("Gilded Rose basic setting", function() {
   });
 
   // Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
-  it("day 15", function() {
-    for(let day = 0; day < 15; day++){
+  it("day 15", function () {
+    for (let day = 0; day < 15; day++) {
       items = gildedRose.updateQuality();
     }
     expect(items[0].name).toBe("basic item");
     expect(items[0].sellIn).toBe(-5);
     expect(items[0].quality).toBe(0);
   });
-  
+
   // La qualité (`quality`) d'un produit ne peut jamais être négative.
-  it("day 20", function() {
-    for(let day = 0; day < 20; day++){
+  it("day 20", function () {
+    for (let day = 0; day < 20; day++) {
       items = gildedRose.updateQuality();
     }
     expect(items[0].name).toBe("basic item");
@@ -54,9 +53,91 @@ describe("Gilded Rose basic setting", function() {
   });
 });
 
+describe("Gilded Rose exceptions", function () {
+  const startItems = [
+    new Item("Aged Brie", 2, 0),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 0, 60),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
 
-xdescribe("Gilded Rose error test", function() {
-  it("create basic item with negative values", function() {
+  ];
+  let gildedRose = new Shop();
+
+  beforeEach(() => {
+    // name, sellIn, quality
+    items = JSON.parse(JSON.stringify(startItems));
+    gildedRose = new Shop(items);
+  });
+
+  it("Aged Brie increase in quality", function () { 
+    for (let day = 0; day < 5; day++) {
+      items = gildedRose.updateQuality();
+    }   
+    expect(items[0].name).toBe("Aged Brie");
+    expect(items[0].sellIn).toBe(-3);
+    expect(items[0].quality).toBe(8); // 2 + 3*2
+  });
+
+  it("Aged Brie quality can not be above 50", function () { 
+    for (let day = 0; day < 30; day++) {
+      items = gildedRose.updateQuality();
+    }   
+    expect(items[0].name).toBe("Aged Brie");
+    expect(items[0].sellIn).toBe(-28);
+    expect(items[0].quality).toBe(50);
+  });
+
+  
+  it("Sulfuras is indeteriorable", function () { 
+    for (let day = 0; day < 50; day++) {
+      items = gildedRose.updateQuality();
+    }   
+    expect(items[1].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[1].sellIn).toBe(0);
+    expect(items[1].quality).toBe(80);
+  });
+ 
+  it("Backstage passes quality increase", function () { 
+    items = gildedRose.updateQuality();
+    expect(items[2].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[2].sellIn).toBe(14);
+    expect(items[2].quality).toBe(21);
+  });
+
+  it("Backstage passes quality increase rapidly", function () { 
+    items = gildedRose.updateQuality();
+    expect(items[3].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[3].sellIn).toBe(9);
+    expect(items[3].quality).toBe(42);
+  });
+
+  it("Backstage passes quality increase rapidly (+3)", function () { 
+    items = gildedRose.updateQuality();
+    expect(items[4].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[4].sellIn).toBe(4);
+    expect(items[4].quality).toBe(43);
+  });
+
+  it("Backstage passes quality fall to 0", function () { 
+    items = gildedRose.updateQuality();
+    expect(items[5].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[5].sellIn).toBe(-1);
+    expect(items[5].quality).toBe(0);
+  });
+
+  it("Backstage passes quality not above 50", function () { 
+    items = gildedRose.updateQuality();
+    expect(items[6].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[6].sellIn).toBe(4);
+    expect(items[6].quality).toBe(50);
+  });
+});
+
+xdescribe("Gilded Rose error test", function () {
+  it("create basic item with negative values", function () {
     // name, sellIn, quality
     items = [new Item("basic item error", -5, -10)];
     gildedRose = new Shop(items);
@@ -66,14 +147,8 @@ xdescribe("Gilded Rose error test", function() {
   });
 });
 
-
 /*
 
-Attendez, ça devient intéressant :
-
-- La qualité (`quality`) d'un produit ne peut jamais être négative.
-- "Aged Brie" augmente sa qualité (`quality`) plus le temps passe.
-- La qualité d'un produit n'est jamais de plus de 50.
 - "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (`quality`)
 - "Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
 
