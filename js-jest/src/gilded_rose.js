6a4729ae3f13f081cd1031a650e0101a9de2fe5d
Guillaume <gcolsenet@coreoz.com>
2021-10-17 14:51:21 +0200

    spelling and cleaning

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,6 +11,7 @@ const ITEM_THAT_INCREASE = [
   "Backstage passes to a TAFKAL80ETC concert",
 ];
 const CONJURED_ITEM = ["Conjured Item", "Conjured Food"];
+const LEGENDARY_ITEM = ["Sulfuras, Hand of Ragnaros"];
 
 class Shop {
   constructor(items = []) {
@@ -21,69 +22,61 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       this.updateQualityOfItem(this.items[i]);
     }
-
     return this.items;
   }
 
   updateQualityOfItem(selectedItem) {
+    // The executions are settled at first
+    if (LEGENDARY_ITEM.includes(selectedItem.name)) {
+      return;
+    }
+    // Time Clock
+    selectedItem.sellIn = selectedItem.sellIn - 1;
+
     // Decrease in quality
     if (!ITEM_THAT_INCREASE.includes(selectedItem.name)) {
       if (selectedItem.quality > 0) {
-        if (selectedItem.name != "Sulfuras, Hand of Ragnaros") {
-          selectedItem.quality = selectedItem.quality - 1;
-        }
+        selectedItem.quality = selectedItem.quality - 1;
         // one additional tick for conjured
         if (CONJURED_ITEM.includes(selectedItem.name)) {
           selectedItem.quality = selectedItem.quality - 1;
         }
       }
-      // Increase in quality
     } else {
-      if (selectedItem.quality < 50) {
-        selectedItem.quality = selectedItem.quality + 1;
-        if (selectedItem.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (selectedItem.sellIn < 11) {
-            if (selectedItem.quality < 50) {
-              selectedItem.quality = selectedItem.quality + 1;
-            }
-          }
-          if (selectedItem.sellIn < 6) {
-            if (selectedItem.quality < 50) {
-              selectedItem.quality = selectedItem.quality + 1;
-            }
-          }
+      // Increase in quality
+      selectedItem.quality = selectedItem.quality + 1;
+      if (selectedItem.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (selectedItem.sellIn < 11) {
+          selectedItem.quality = selectedItem.quality + 1;
         }
-      }
-    }
-    // Time clock
-    if (selectedItem.name != "Sulfuras, Hand of Ragnaros") {
-      selectedItem.sellIn = selectedItem.sellIn - 1;
-    }
-    // Specific rulls for over time actions
-    if (selectedItem.sellIn < 0) {
-      if (!ITEM_THAT_INCREASE.includes(selectedItem.name)) {
-        if (selectedItem.quality > 0) {
-          if (selectedItem.name != "Sulfuras, Hand of Ragnaros") {
-            selectedItem.quality = selectedItem.quality - 1;
-          }
-          // one additional tick for conjured
-          if (CONJURED_ITEM.includes(selectedItem.name)) {
-            selectedItem.quality = selectedItem.quality - 1;
-          }
-        } else {
-          selectedItem.quality = selectedItem.quality - selectedItem.quality;
-        }
-      } else {
-        if (selectedItem.quality < 50) {
+        if (selectedItem.sellIn < 6) {
           selectedItem.quality = selectedItem.quality + 1;
         }
       }
-      if (selectedItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
-        selectedItem.quality = 0; // Concert is passeds 
+    }
+
+    // Specific rules for over time actions
+    if (selectedItem.sellIn < 0) {
+      if (!ITEM_THAT_INCREASE.includes(selectedItem.name)) {
+        selectedItem.quality = selectedItem.quality - 1;
+        // one additional tick for conjured
+        if (CONJURED_ITEM.includes(selectedItem.name)) {
+          selectedItem.quality = selectedItem.quality - 1;
+        }
+      } else {
+        selectedItem.quality = selectedItem.quality + 1;
+      }
+      if (selectedItem.name == "Backstage passes to a TAFKAL80ETC concert") {
+        selectedItem.quality = 0; // Concert is passeds
       }
     }
+
+    // Limits enforeced
     if (selectedItem.quality < 0) {
       selectedItem.quality = 0; // Quality can not be negative
+    }
+    if (selectedItem.quality > 50) {
+      selectedItem.quality = 50; // Quality can not be above 50
     }
   }
 }
